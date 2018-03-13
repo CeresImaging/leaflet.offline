73837455a463fdfa84484bdbe314480b226fa2d7
evavro <me@madhax.io>
2018-03-13 16:53:58 -0700

    cleanup, added support for Point shape intersections

--- src/GeoUtils.js
+++ src/GeoUtils.js
@@ -1,71 +1,15 @@
 import turf from '@turf/turf'
 
 export function coordsIntersectPolygon (coords, shape) {
-  console.log('--- detecting coord intersection', coords)
 	const point = turf.point(coords)
-
-  // console.log('------ coord as point', point)
 
 	return turf.inside(point, shape)
 }
 
-// TODO: support Point, LineString
-// FIXME: frankenstein code, improve (replace with `Array.prototype.any`)
-export function polygonsIntersect (shape1, shape2) {
-  let result = []
-
-  if (shape1.type == 'Polygon' || shape1.type == 'MultiLineString') {
-    result = shape1.coordinates.reduce((dump, part) => {
-      return dump.concat(part.reduce((dump, coord) => {
-        return coordsIntersectPolygon(coord, shape2)
-      }))
-    }, [])
-  } else if (shape1.type == 'MultiPolygon') {
-    result = shape1.coordinates.reduce((dump, poly) => {
-      return dump.concat(poly.reduce((points, part) => {
-        return coordsIntersectPolygon(part, shape2)
-      }, []))
-    }, [])
-  } else if (shape1.type == 'Feature') {
-		result = polygonsIntersect(shape1.geometry, shape2)
-  } else if (shape1.type == 'GeometryCollection') {
-    result = shape1.geometries.reduce((dump, g) => {
-			return dump.concat(polygonsIntersect(g, shape2))
-    }, [])
-  } else if (shape1.type == 'FeatureCollection') {
-    result = shape1.features.reduce((dump, f) => {
-			return dump.concat(polygonsIntersect(f, shape2))
-    }, [])
-		// TODO: determine if `any` element of the dump is `true`
-  }
-
-  // console.log('util results', result[0])
-
-  return result[0]
-}
-
-// --- REMOVE BELOW (it works but only for polygons and multi-polygons)
-
-export function polyIntersectsWithMultiPoly (poly, multiPoly) {
-  let intersects = false
-
-  poly.coordinates.forEach(coord1 => {
-    coord1.forEach(coord2 => {
-      const polyPoint = turf.point(coord2)
-
-      if (turf.inside(polyPoint, multiPoly) && !intersects) {
-        intersects = true
-      }
-    })
-  })
-
-  return intersects
-}
-
-// --- NEW
-
 export function shapesIntersect (shape1, shape2) {
-  if (shape1.type == 'Polygon' || shape1.type == 'MultiLineString') {
+  if (shape1.type == 'Point') {
+    return coordsIntersectPolygon(shape1.coordinates, shape2)
+  } else if (shape1.type == 'Polygon' || shape1.type == 'MultiLineString') {
     return shape1.coordinates.find(coord1 => {
       return coord1.find(coord2 => {
         return coordsIntersectPolygon(coord2, shape2)
--- src/TileLayerOffline.js
+++ src/TileLayerOffline.js
@@ -2,90 +2,7 @@ import L from 'leaflet';
 import localforage from './localforage';
 import geoBox from 'geojson-bbox';
 import { tileToGeoJSON } from '@mapbox/tilebelt';
-import { polygonsIntersect, polyIntersectsWithMultiPoly, shapesIntersect } from './GeoUtils';
-
-// https://codepen.io/slurmulon/pen/zRVzjX
-//
-
-// REMOVE ME (for testing)
-
-// --------------------------- START
-
-function createPolygonFromBounds (latLngBounds, map) {
-  const center = latLngBounds.getCenter()
-  const latlngs = []
-
-  latlngs.push(latLngBounds.getSouthWest()) // bottom left
-  latlngs.push({ lat: latLngBounds.getSouth(), lng: center.lng }) // bottom center
-  latlngs.push(latLngBounds.getSouthEast()) // bottom right
-  latlngs.push({ lat: center.lat, lng: latLngBounds.getEast() }) // center right
-  latlngs.push(latLngBounds.getNorthEast()) // top right
-  latlngs.push({ lat: latLngBounds.getNorth(), lng: map.getCenter().lng }) // top center
-  latlngs.push(latLngBounds.getNorthWest()) // top left
-  latlngs.push({ lat: map.getCenter().lat, lng: latLngBounds.getWest() }) // center left
-
-  return new L.polygon(latlngs)
-}
-
-// ---
-
-// var d2r = Math.PI / 180,
-//   r2d = 180 / Math.PI;
-
-// /**
-//  * Get the bbox of a tile
-//  *
-//  * @name tileToBBOX
-//  * @param {Array<number>} tile
-//  * @returns {Array<number>} bbox
-//  * @example
-//  * var bbox = tileToBBOX([5, 10, 10])
-//  * //=bbox
-//  */
-// function tileToBBOX(tile) {
-//   var e = tile2lon(tile[0] + 1, tile[2]);
-//   var w = tile2lon(tile[0], tile[2]);
-//   var s = tile2lat(tile[1] + 1, tile[2]);
-//   var n = tile2lat(tile[1], tile[2]);
-//   return [w, s, e, n];
-// }
-
-// /**
-//  * Get a geojson representation of a tile
-//  *
-//  * @name tileToGeoJSON
-//  * @param {Array<number>} tile
-//  * @returns {Feature<Polygon>}
-//  * @example
-//  * var poly = tileToGeoJSON([5, 10, 10])
-//  * //=poly
-//  */
-// function tileToGeoJSON(tile) {
-//   var bbox = tileToBBOX(tile);
-//   var poly = {
-//     type: 'Polygon',
-//     coordinates: [[
-//       [bbox[0], bbox[1]],
-//       [bbox[0], bbox[3]],
-//       [bbox[2], bbox[3]],
-//       [bbox[2], bbox[1]],
-//       [bbox[0], bbox[1]]
-//     ]]
-//   };
-//   return poly;
-// }
-
-// function tile2lon(x, z) {
-//   return x / Math.pow(2, z) * 360 - 180;
-// }
-
-// function tile2lat(y, z) {
-//   var n = Math.PI - 2 * Math.PI * y / Math.pow(2, z);
-//   return r2d * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)));
-// }
-
-// END -----------------------------
-
+import { shapesIntersect } from './GeoUtils';
 
 /**
  * A layer that uses store tiles when available. Falls back to online.
@@ -168,15 +85,10 @@ const TileLayerOffline = L.TileLayer.extend(/** @lends  TileLayerOffline */ {
       this._map.project(bounds.getSouthEast(), zoom)
     )
 
-    console.log(`[leaflet.offline] getTileUrls latLngBounds @ [zoom: ${zoom}]`, bounds)
-    console.log(`[leaflet.offline] getTileUrls pointBounds @ [zoom: ${zoom}]`, pointBounds)
-
     const tileBounds = L.bounds(
       pointBounds.min.divideBy(this.getTileSize().x).floor(),
       pointBounds.max.divideBy(this.getTileSize().x).floor()
     )
-
-    console.log(`[leaflet.offline] getTileUrls tileBounds @ [zoom: ${zoom}]`, tileBounds)
 
     let url
 
@@ -199,7 +111,7 @@ const TileLayerOffline = L.TileLayer.extend(/** @lends  TileLayerOffline */ {
     return tiles
   },
   /**
-   * Provides the tile URLs for a non-rectangular shape (GeoJSON) at the provided zoom level
+   * Dermines the tile URLs for a non-rectangular shape (GeoJSON) at the defined zoom level
    */
   getTileUrlsInShapes (shapes, zoom) {
     const tiles = []
@@ -209,18 +121,16 @@ const TileLayerOffline = L.TileLayer.extend(/** @lends  TileLayerOffline */ {
     this.setUrl(this._url.replace('{z}', zoom), true)
 
     geometries.forEach(shape => {
-      // --- start
-
-      const boundCoords  = geoBox(shape)
-      const boundLatLngs = new L.latLngBounds(L.GeoJSON.coordsToLatLngs([[boundCoords[0], boundCoords[1]], [boundCoords[2], boundCoords[3]]]))
-      const boundShape = createPolygonFromBounds(boundLatLngs, this._map).setStyle({ color: 'yellow' })
+      const boundCoords = geoBox(shape)
+      const boundLatLngs = new L.latLngBounds(L.GeoJSON.coordsToLatLngs([
+        [boundCoords[0], boundCoords[1]],
+        [boundCoords[2], boundCoords[3]]
+      ]))
 
       const pointBounds = L.bounds(
         this._map.project(boundLatLngs.getNorthWest(), zoom),
         this._map.project(boundLatLngs.getSouthEast(), zoom)
       )
-
-      // --- end
 
       // NOTE: these values match the Codepen (correct)
       const tileBounds = L.bounds(
@@ -228,32 +138,15 @@ const TileLayerOffline = L.TileLayer.extend(/** @lends  TileLayerOffline */ {
         pointBounds.max.divideBy(this.getTileSize().x).floor()
       )
 
-      console.log('@@@ tile bounds', tileBounds)
-
       let url
-
-      // boundShape.addTo(this._map)
 
       for (let j = tileBounds.min.y; j <= tileBounds.max.y; j += 1) {
         for (let i = tileBounds.min.x; i <= tileBounds.max.x; i += 1) {
-          // FIXME: tilePoint `x` and `y` are slightly off
-          //  - actually at second glance these values are okay. even the resulting lat/lng looks fine...?
           const tilePoint = new L.Point(i, j)
-          const tileShape = tileToGeoJSON([tilePoint.x, tilePoint.y, zoom]) // FIXME: the 0th element of each coordinate Array is off here. 1st element is fine.
-          // const tileIntersects = polygonsIntersect(tileShape, shape)
-          // const tileIntersects = polyIntersectsWithMultiPoly(tileShape, shape)
+          const tileShape = tileToGeoJSON([tilePoint.x, tilePoint.y, zoom])
           const tileIntersects = shapesIntersect(tileShape, shape)
 
-          console.log('\n--- tile point', tilePoint)
-          console.log('--- tile x/y (orig)', i, j)
-          console.log('--- tile shape', tileShape)
-
           if (tileIntersects) {
-            // console.log('[leaflet.offline] added tile point (in shape!)', tilePoint)
-            // console.log('[leaflet.offline] added tile shape', tileShape)
-
-            L.geoJSON(tileShape, { style: { color: 'orange' } }).addTo(this._map)
-
             url = L.TileLayer.prototype.getTileUrl.call(this, tilePoint)
 
             tiles.push({
